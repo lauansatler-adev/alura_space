f5b588daf508f7714bdb229354e1cd5a501d5763
Lauan Satler <lauanesatlert@gmail.com>
2026-09-23 17:12:41 +0100

    Salvando um cadastro na base de dados

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User # Importa a tabela de usuários que fica dentro do django
 
 def login(request):
     
@@ -12,7 +13,26 @@ def cadastro(request):
     if request.method == "POST":
         form = CadastroForms(request.POST)
         
-        if form["senha_1"].value() != form["senha_2"].value():
-            return redirect("cadastro")
+        if form.is_valid():
+            if form["senha_1"].value() != form["senha_2"].value(): #Verifica se as senhas são diferentes
+                return redirect("cadastro") # Retornando para a página de cadastro caso as senhas sejam diferentes
+            
+            """ Pegando as informações passadas e colocando dentrode variáveis para organizar o código"""
+            nome = form["nome_cadastro"].value()
+            email = form["email"].value()
+            senha = form["senha_1"].value()
+            
+            if User.objects.filter(username=nome).exists(): # Verifica se já existe um usuário com esse nome na tabela do django
+                return redirect("cadastro") # Retornando para a página de cadastro caso já exista um usuário com esse nome
+            
+            usuario = User.objects.create_user(
+                username=nome,
+                email=email,
+                password=senha
+            ) # Criando um usuário com as informações passadas na página de cadastro
+            
+            usuario.save() # Salvando o usuário no banco de dados
+            
+            return redirect("login") # Redireciona para a página de login se todos os dados estiverem corretos
         
     return render(request, "usuarios/cadastro.html", {"form": form})
